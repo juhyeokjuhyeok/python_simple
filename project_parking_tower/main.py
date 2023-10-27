7d38f634b4b4676efa66cd3ebbe8e3ff5d2d2efd
juhyeokjuhyeok <134502063+juhyeokjuhyeok@users.noreply.github.com>
2023-10-27 13:36:04 +0900

    Add files via upload

--- project_parking_tower/main.py
+++ project_parking_tower/main.py
@@ -35,27 +35,43 @@ while True:
 
     while True:
         select_num = int(input(">>번호: "))
-        if 4>= select_num >= 1:
+        if 4 >= select_num >= 1:
             break
         else:
             print("MSG: 올바른 번호를 입력하세요. ")
 
     if select_num == 1:     #입고
         pass
-        # 주차타워 여유공간 Check
-        if car_cnt < max_car: # 입고가능
+        # 주차 타워 여유 공간 Check
+        if car_cnt < max_car: # 입고 가능
             car_num = input(">>입고: ")
             for i, car in enumerate(tower):
                 if car == "":
-                    car_cnt +=1
+                    tower[i] = car_num
+                    car_cnt += 1
                     break
         else:
             print("MSG: 만차입니다. 다음에 이용해주세요")
         # 입고할 차량 번호 입력
         # 입고
     elif select_num == 2:   #출고
-        pass
+        #1.출고 차량번호 입력
+        car_num = input(">>출고: ")
+        if car_num in tower:
+            for i, car in enumerate(tower):
+                if car == car_num:
+                    tower[i] = "" #차량 제거
+                    car_cnt -= 1 # 현재 주차대수 동기화
+                    break
+        else:
+            print("MSG: 해당 번호로 입고된 차량이 없습니다. ")
+        #2.tower == 출고 차량번호 match
+        # 3-1:yes 출고(tower 제거(""), car_cnt-1)
+        # 3-2 no: MSG:(칩고된 차량 X)
     elif select_num == 3:   #조회
-        pass
+        print("== 주차 타워 현황 ==")
+        for i in range(len(tower)-1, -1, -1):
+            print(f" > {i+1}층 {tower[i]}")
     elif select_num == 4:
         print("MSG: 프로그램을 종료합니다. ")
+        exit()
