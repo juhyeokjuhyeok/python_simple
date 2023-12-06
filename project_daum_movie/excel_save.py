fe0af9ad169fd0ac2662cbcbc4906674a31a2fbe
juhyeokjuhyeok <134502063+juhyeokjuhyeok@users.noreply.github.com>
2023-12-06 16:54:09 +0900

    Add files via upload

--- project_daum_movie/excel_save.py
+++ project_daum_movie/excel_save.py
@@ -1,13 +1,14 @@
 #  DB에 저장되어있는 수집 데이터를 EXCEL로 저장
 # Python(Dice, List 컬렉션)
 # ‡
-#Pandas의 DataFrame(표)
+# Pandas의 DataFrame(표)
 #
-#File(CSV, EXCEL)
+# File(CSV, EXCEL)
 
 
 import pandas as pd
 from db.movie_dao import get_reviews
+from datetime import datetime
 reviews = get_reviews()
 
 for item in reviews:
